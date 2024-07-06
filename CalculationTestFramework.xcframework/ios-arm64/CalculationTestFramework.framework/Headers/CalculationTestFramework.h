f0a466ad3da2fdca16300ea0c530c1b92e7cfb4c
Kostadina Geceska <kostadinag@nextsense.com>
2024-07-06 14:36:13 +0200

    initial message

---
+++ CalculationTestFramework.xcframework/ios-arm64/CalculationTestFramework.framework/Headers/CalculationTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  CalculationTestFramework.h
+//  CalculationTestFramework
+//
+//  Created by Kostadina Gecevska on 6.7.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalculationTestFramework.
+FOUNDATION_EXPORT double CalculationTestFrameworkVersionNumber;
+
+//! Project version string for CalculationTestFramework.
+FOUNDATION_EXPORT const unsigned char CalculationTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculationTestFramework/PublicHeader.h>
+
+
